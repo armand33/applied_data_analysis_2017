45e54e743678e5b4096ac075f2a963a9ba614933
Cyril Wendl <cyrilwendl@gmail.com>
2017-09-23 23:15:53 +0200

    Updated homework code

--- 00 - Optional homework/Optional Homework 1.py
+++ 00 - Optional homework/Optional Homework 1.py
@@ -1,51 +1,49 @@
 import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+import numpy as np
 
 # do everything with standard Python libraries
-def nativeversion():
-    import csv
-
-    # Open CSV
-    table=[]
+data = []
+data_pd = []
+def prep_data():
     with open('full_data.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        data=dict()
-        for fname in reader.fieldnames:  #TODO rewrite code more elegant
-            data[fname]=[] # initialize
-
+        reader = csv.reader(csvfile)
         for row in reader:
-            table.append(row)
-            for fname in reader.fieldnames:
-                data[fname].append(row[fname])
-    print(table)
+            data.append(row)
+        header=data[0]
+        print(header)
+        data.pop(0)
+    data_pd=pd.DataFrame(data, columns=header)
+    data_pd["age"] = data_pd["age"].replace("NA", 0).astype('int') # data type int for "age"
+    data_pd["year"] = data_pd["year"].replace("NA", 0).astype('int')  # data type int for "age"
+    return data_pd
 
-    # GET NUMBERS FROM ARTICLE
-    # percentage suicide
-    perc_suicide=data['intent'].count("Suicide") / len(data['intent'])*100 # percentage of suicides
-    print("Percentage Suicides: " + repr(round(perc_suicide, 2)) + " %")
+def get_numbers(data_pd):
 
-    # percentage male suicides
+    # Get numbers from article
+    # 1. suicides
+    perc_intent=data_pd[['intent','sex','age']].groupby(['intent']).count().apply(lambda x: 100*x/ x.sum())
+    #print(round(perc_intent,2))
 
-    print("Percentage Males in Suicides: " + repr(round(perc_suicide_male, 2)) + " %")
+    # 2. men in suicide category
+    data_pd_intent=data_pd[data_pd["intent"] == "Suicide"]
+    perc_intent_sex=data_pd_intent[['intent', 'sex', 'age']].groupby(['sex']).count().apply(lambda x: 100 * x / x.sum())
+    #print(round(perc_intent_sex,2))
 
+    # 3. pecentage of men aged 45 or older in suicides
+    perc_intent_sex_age=data_pd_intent[data_pd_intent["age"] >= 45].count() / data_pd_intent.count()
+    #print(round(perc_suicide_sex_age[0],2))
 
+    # 4. total homicides / year
+    data_pd_homicide=data_pd[data_pd["intent"] == "Homicide"]
+    sum_intent = data_pd_homicide[['intent', 'age','year']].groupby(['year']).sum() # TODO I get other numbers
+    #print(sum_intent)
 
+    # TODO where is victim data?
 
-# do the same with a more sophisticated library
-def pandaversion():
-    import pandas
-
-    # Load the data into a DataFrame
-    data = pandas.read_csv('full_data.csv')
-
-    ## Group by intent
-    print(data.groupby('intent').sum())
-
-
-    ## Group by intent, sex
-    data_agg_intent_sex=data.groupby(['intent','sex']).count()
-    #print(data_agg_intent_sex.groupby(level=0).apply(lambda x: 100*x/ x.sum()))
+    #perc_men_suicide = data_pd_suicide[['intent', 'sex', 'age']].groupby(['sex']).count().apply(lambda x: 100 * x / x.sum())  # get all intents in percent
 
 
-
-#nativeversion()
-pandaversion()
+data_pd=prep_data()
+get_numbers(data_pd)
